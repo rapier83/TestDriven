869f3cbbb9eadb63e8578db929fac9693f16b7c1
Junmo Kang <rapier83@gmail.com>
2017-06-27 13:32:54 +0900

    템플릿을 통한 HTML 파일 검사/제작 Type Error: '...iterable..' -> `for row in rows` 구문에서 나온 에러같은데 왜 그러는지 모르것다

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,17 +19,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # The side show 'To-do' in the title and header.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        # MARK: legacy -- self.fail('Finish the test!')
+        HeaderText = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', HeaderText)
 
         # She finally decide to add work to do in the app.
+        InputBox = self.browser.find_element_by_id('id-new-item')
+        self.assertEqual(
+            InputBox.get_attribute('placeholder'),
+            'Input work to do'
+        )
+
         # In textbox, she input "Buying peacock feather"
         # (Edith's hobby is making a web to catch a flying fish)
+        InputBox.send_keys('Buying peacock feather')
 
         # When hit the Enter, the page reloaded and...
         # "1: Buying peacock feather" item added in the work list.
+        InputBox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id-list-table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buying peacock feather' for row in rows),
+            "Doesn't show the new To-do in table"
+        )
 
         # There are another text-boxes which to additional items in the page.
-        # Input : "Making the web by using the peacock feather" (Edith is organized person)
+        # Input : "Making the web by using the peacock feather" (Edith is a very organized person)
+        self.fail('Finish the test!')
+
 
         # The page will reload again, Two items will be in the work list.
         # Edith wonder if the site has input list.
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def HomePage(request):
-    return HttpResponse(b'<html><title>To-Do Lists</title></html>')
+    return render(request,'home.html')
